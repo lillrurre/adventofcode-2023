f3968c890e06c0d1cff6f2ffd3d5630fc7006644
Erik Ehrström <erik.ehrstrom@gmail.com>
2023-12-03 19:20:12 +0200

    simplify

--- 3/3.go
+++ 3/3.go
@@ -15,7 +15,6 @@ type coordinate struct {
 var adj = []coordinate{{-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {0, -1}, {1, 1}, {1, 0}, {1, -1}}
 
 func main() {
-
 	b, _ := os.ReadFile("input/3")
 	lines := strings.Split(string(b), "\n")
 
@@ -26,49 +25,48 @@ func main() {
 	syms := make(map[coordinate]string)
 
 	for y, line := range lines {
-		matches := numRe.FindAllString(line, -1)
-		for _, s := range matches {
+		for _, s := range numRe.FindAllString(line, -1) {
 			n, _ := strconv.Atoi(s)
 			nums[coordinate{x: strings.Index(line, s), y: y}] = n
 			line = strings.Replace(line, s, strings.Repeat(".", len(s)), 1)
 		}
-		matches = symRe.FindAllString(line, -1)
-		for _, s := range matches {
+		for _, s := range symRe.FindAllString(line, -1) {
 			syms[coordinate{x: strings.Index(line, s), y: y}] = s
 			line = strings.Replace(line, s, strings.Repeat(".", len(s)), 1)
 		}
 	}
 
-	// part 1
-	{
-		sum1 := 0
-		sum2 := 0
-		for symCoord, sym := range syms {
-			var prev int
-			for numCoord, num := range nums {
-				if !numCoord.adjacent(len(fmt.Sprintf("%d", num)), symCoord) {
-					continue
-				}
-				sum1 += num
-				if sym != "*" {
-					continue
-				}
-				if prev == 0 {
-					prev = num
-					continue
-				}
-				sum2 += prev * num
+	sum1, sum2 := 0, 0
+	for symCoord, sym := range syms {
+		var prev int
+		for numCoord, num := range nums {
+			if !numCoord.adjacent(num, symCoord) {
+				continue
 			}
-			prev = 0
+			sum1 += num
+			if sym != "*" {
+				continue
+			}
+			if prev == 0 {
+				prev = num
+				continue
+			}
+			sum2 += prev * num
 		}
-		fmt.Printf("[1] Result: %d\n", sum1)
-		fmt.Printf("[2] Result: %d\n", sum2)
+		prev = 0
 	}
+	fmt.Printf("[1] Result: %d\n", sum1)
+	fmt.Printf("[2] Result: %d\n", sum2)
 }
 
 func (c *coordinate) adjacent(n int, other coordinate) bool {
+	var digits int
+	for n != 0 {
+		n /= 10
+		digits++
+	}
 	for _, coord := range adj {
-		for i := 0; i < n; i++ {
+		for i := 0; i < digits; i++ {
 			if other.x == coord.x+c.x+i && other.y == coord.y+c.y {
 				return true
 			}
