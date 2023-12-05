5155c6f7f01c426ce1c536ea54e1a364990cc4bb
Erik Ehrström <erik.ehrstrom@gmail.com>
2023-12-05 02:34:43 +0200

    bash for fun

---
+++ 4/4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# part 1
+sum=0
+while IFS= read -r line
+do
+    result=$(echo "$line" | cut -d ':' -f2 | rg --pcre2 -o '\b(\d+)\b(?=.*\b\1\b)' | wc -l | awk '{ result = 0; for (i = 1; i <= $0; i++) { if (result == 0) result = 1; else result *= 2 } print result }')
+    ((sum += "$result"))
+done < "../input/4"
+echo "$sum"
