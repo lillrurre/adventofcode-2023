5bcd3342bf7ab6ac15d04c0ba7a33134c6dcf6f7
Erik Ehrström <erik.ehrstrom@gmail.com>
2023-12-13 00:59:18 +0200

    Small update

--- 12/src/main.rs
+++ 12/src/main.rs
@@ -6,58 +6,51 @@ fn main() {
 
     let records: Vec<&str> = lines.iter().map(|line| line.split_whitespace().next().unwrap()).collect();
 
-    let groups: Vec<Vec<u8>> = lines.iter().map(|line| {
-        line.split_whitespace()
+    let groups: Vec<Vec<u8>> = lines.iter()
+        .map(|line| line.split_whitespace()
             .nth(1).unwrap()
             .split(',')
             .map(|num| num.parse().unwrap())
             .collect()
-    }).collect();
+    ).collect();
 
-    let mut sum: u32 = 0;
-    for (record, group) in records.iter().zip(groups.iter()) {
-        sum += solve(record, group);
-    }
+    let sum: u32 = records.iter()
+        .zip(groups)
+        .map(|(record, group)| solve(record, group.as_slice()))
+        .sum();
 
-    println!("Total result: {}", sum);
+    println!("[1] Result: {sum}");
 }
 
 fn solve(record: &str, group: &[u8]) -> u32 {
-    if record.is_empty() {
-        if group.is_empty() {
-            return 1;
+    return match (record.is_empty(), group.is_empty()) {
+        (true, true) => 1,
+        (false, true) => if record.contains("#") { 0 } else { 1 },
+        (true, false) => 0,
+        _ => {
+            let first_spring = record.chars().next().unwrap().to_ascii_lowercase();
+            let first_num = group[0] as usize;
+
+            let res = match first_spring {
+                '?' => solve(&record[1..], group),
+                _ => 0,
+            };
+            return match first_spring {
+                '.' => solve(&record[1..], group),
+                _ => {
+                    if record.len() < first_num
+                        || record[..first_num].contains('.')
+                        || first_num != record.len() && record.chars().nth(first_num).unwrap() == '#' {
+                        return res;
+                    }
+
+                    if first_num == record.len() {
+                        return res + solve("", &group[1..]);
+                    }
+
+                    return res + solve(&record[first_num + 1..], &group[1..]);
+                }
+            };
         }
-        return 0;
-    }
-
-    if group.is_empty() {
-        if record.chars().all(|c| c != '#') {
-            return 1;
-        }
-        return 0;
-    }
-
-    let first_spring = record.chars().next().unwrap().to_ascii_lowercase();
-    let first_num = group[0] as usize;
-    let mut res = 0;
-
-    if first_spring == '.' {
-        return solve(&record[1..], group);
-    }
-
-    if first_spring == '?' {
-        res += solve(&record[1..], group);
-    }
-
-    if record.len() < first_num
-        || record[..first_num].contains('.')
-        || first_num != record.len() && record.chars().nth(first_num).unwrap() == '#' {
-        return res;
-    }
-
-    if first_num == record.len() {
-        return res + solve("", &group[1..]);
-    }
-
-    return res + solve(&record[first_num + 1..], &group[1..]);
+    };
 }
