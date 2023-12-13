9bcb047746a7441ef1950d33ba4537dcee120551
Erik Ehrström <erik.ehrstrom@gmail.com>
2023-12-14 00:33:26 +0200

    Test stuff

--- 12/src/main.rs
+++ 12/src/main.rs
@@ -1,7 +1,12 @@
 use std::fs::read_to_string;
 
 fn main() {
-    let content = read_to_string("src/input.txt").unwrap();
+    let sum = part1("src/input.txt");
+    println!("[1] Result: {sum}");
+}
+
+fn part1(input_file: &str) -> u32 {
+    let content = read_to_string(input_file).unwrap();
     let lines: Vec<&str> = content.lines().collect();
 
     let records: Vec<&str> = lines.iter().map(|line| line.split_whitespace().next().unwrap()).collect();
@@ -12,14 +17,14 @@ fn main() {
             .split(',')
             .map(|num| num.parse().unwrap())
             .collect()
-    ).collect();
+        ).collect();
 
     let sum: u32 = records.iter()
         .zip(groups)
         .map(|(record, group)| solve(record, group.as_slice()))
         .sum();
 
-    println!("[1] Result: {sum}");
+    return sum
 }
 
 fn solve(record: &str, group: &[u8]) -> u32 {
@@ -53,4 +58,22 @@ fn solve(record: &str, group: &[u8]) -> u32 {
             };
         }
     };
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::part1;
+
+    #[test]
+    fn test() {
+        let sum = part1("src/input.txt");
+        assert_eq!(sum, 7163);
+    }
+
+    #[test]
+    fn test_sample() {
+        let sum = part1("src/input_sample.txt");
+        assert_eq!(sum, 21);
+    }
+
 }
